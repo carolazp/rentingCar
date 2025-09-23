0c520b1a41eefdaf19429714c495672d1fe6e9a6
carolazp <ca.azparrent@gmail.com>
2025-09-23 17:38:30 +0200

    feat: create maven project and car class
    extra done: test car objects created

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,5 +9,23 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "HELlO MOTO!!!!" );
+        int mateo = 2;
+        System.out.println("This is mateo: "+ mateo);
+
+        Car bmw001 = new Car("1", "BMW", "M3", "1234", 2022, 100.0);
+        System.out.print("This is my BMW: "+ bmw001.toString());
+
+        System.out.print("\n" + bmw001.getBrand());
+
+        Car bmw003 = new Car();
+        bmw003.setBrand("BMW");
+        System.out.print("\n"+ bmw003.toString());
+
+        bmw001.setBrand("Mercedes");
+        System.out.print("\n"+ bmw001.toString());
+
+        // The next line will throw an error because the constructor is not defined
+        // Car bmw004 = new Car("3", "BMW");
+
     }
 }
---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,91 @@
+package org.example;
+
+public class Car {
+
+    // Construyendo/definiendo la clase con sus parametros
+    private String id;
+    private String brand;
+    private String model;
+    private String plate;
+    private int year;
+    private double price;
+
+    public Car(String id, String brand, String model, String plate, int year, double price){
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.plate = plate;
+        this.year = year;
+        this.price = price;
+    }
+
+    // los metodos propios de la clase Car
+    // 1º getters y setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // 2º otros metodos
+    public Car() {}
+
+    public Car(String id){
+        this.id = id;
+    }
+
+    @Override
+    public String toString(){
+        return "Car{" +
+                "id='" + id + '\'' +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", plate='" + plate + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                '}';
+    }
+}
