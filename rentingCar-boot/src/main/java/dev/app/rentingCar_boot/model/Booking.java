30c1338736de2d077a7fe2d72b9dbe5942c9ae62
carolazp <ca.azparrent@gmail.com>
2025-10-18 14:46:34 +0200

    feat: create Client and Booking model

--- rentingCar-boot/src/main/java/dev/app/rentingCar_boot/model/Booking.java
+++ rentingCar-boot/src/main/java/dev/app/rentingCar_boot/model/Booking.java
@@ -1,5 +1,11 @@
 package dev.app.rentingCar_boot.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Booking {
 
+    @Id
+    private String id;
 }
--- rentingCar-boot/src/main/java/dev/app/rentingCar_boot/model/Client.java
+++ rentingCar-boot/src/main/java/dev/app/rentingCar_boot/model/Client.java
@@ -1,6 +1,12 @@
 package dev.app.rentingCar_boot.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Client {
 
+    @Id
+    private String id;
 
 }
