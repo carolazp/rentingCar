9e5d9e6f4c4d34f02f72fe183a65d5db22e8bbc5
carolazp <ca.azparrent@gmail.com>
2025-10-18 11:39:27 +0200

    feat: create CarController and its template cars.html

---
+++ rentingCar-boot/src/main/java/dev/app/rentingCar_boot/controller/CarController.java
@@ -0,0 +1,27 @@
+package dev.app.rentingCar_boot.controller;
+
+import dev.app.rentingCar_boot.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CarController{
+
+    @Autowired // injects an instance of CarRepository into the controller
+    CarRepository carRepository;
+
+    @GetMapping("/cars")
+    public String listCar(Model model){
+        model.addAttribute("cars", carRepository.findAll());
+
+        System.out.println("Cars: " + carRepository.findAll());
+
+        return "cars"; // This is cars.html ( src.main.resources.templates. "CARS.HTML" )
+
+    }
+
+
+}
